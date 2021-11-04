1b4385622b4db059a506971cdcd34202417ee00d
SJ <restart96@gmail.com>
2021-11-04 15:12:50 +0900

    UserInterface에 orders / latestOrder 추가

--- php/src/app/Domain/User/Contracts/UserInterface.php
+++ php/src/app/Domain/User/Contracts/UserInterface.php
@@ -4,5 +4,13 @@ namespace App\Domain\User\Contracts;
 
 interface UserInterface
 {
+    /**
+     * 회원의 주문 정보를 반환합니다.
+     */
+    public function orders();
 
+    /**
+     * 회원의 가장 최근 주문 정보를 반환합니다.
+     */
+    public function latestOrder();
 }
